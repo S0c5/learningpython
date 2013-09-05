2333ed07f5f00ee23beb892df10a401a8a8c443e
Omar David barinas <dbarinas@glive3d.com>
2013-09-05 13:41:44 -0500

    segundo ejercicio
    this is my second exercice, comments in #

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# this is my fist comment in my exercices, Hello mother, okay my mother does not view my repository.
+# Ahora si probando 1,2,3 Comentarios Siiiiiiiiiii :) 
+
+
+print "esto se muestra" # "esto no se muestra"
+
+
+# ahora temrine mi segundo ejercicio :3
