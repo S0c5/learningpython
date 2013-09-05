a1c03ae43c6bed4a6e93565012a4fe23aa5c850e
Omar David barinas <dbarinas@glive3d.com>
2013-09-05 13:55:42 -0500

    ex4.py
    names of varibles

---
+++ ex4.py
@@ -0,0 +1,19 @@
+# Names Of variables
+
+
+name = "Omar david"
+last_name = "Barinas"
+years=17
+
+
+print "My name is ", name, "and my last name is ", last_name, " and i am ",years," old"
+
+
+# :) varibales koool :) 
+
+
+c = 0 # 
+i = 0 # Coutners
+j = 0 #
+
+
