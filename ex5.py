3ae78384a0756765a9c319f8820ee87981ac013f
Omar David barinas <dbarinas@glive3d.com>
2013-09-05 14:02:29 -0500

    Create ex5.py
    Other form for print.

---
+++ ex5.py
@@ -0,0 +1,7 @@
+# this is the other form   print  
+
+
+print "this is a number %d"%10
+print "this is a float %r"%4.9
+print "this is a string %s"%"string every day every night"
+print "this is a many variables group,%s,%d,%r"%("september",05,2.01)
