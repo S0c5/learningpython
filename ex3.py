14a2ad17fac23d3ec11b3ccc808803755ec599f7
Omar David barinas <dbarinas@glive3d.com>
2013-09-05 13:46:07 -0500

    Create ex3.py
    Yes numbers and math.

---
+++ ex3.py
@@ -0,0 +1,13 @@
+print "Numbers and math"
+
+
+print "this is true"
+print 2*2<3+4
+
+print "hello word sum"
+print 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2
+
+print "Okay, other float numbers :D "
+print 0.5 * 0.2
+
+
