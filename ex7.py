cc971f54607727e1aa549c3a002b7a67000c3961
Omar David barinas <dbarinas@glive3d.com>
2013-09-05 14:11:40 -0500

    Create ex7.py

---
+++ ex7.py
@@ -0,0 +1,10 @@
+# MMMM other exervice form print and concatenation of strings
+
+
+ch1 = "g"
+ch2 = "o"
+ch3 = "o"
+ch4 = "d"
+print ":D"*100 # is very happy
+
+print "alice is " + ch1 + ch2 + ch3 + ch4
