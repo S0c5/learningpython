dbdc15f0643a45176f874e65b7d14e8760aa8401
Omar David barinas <dbarinas@glive3d.com>
2013-09-05 21:55:40 -0500

    Create ex9.py
    example of print :)

---
+++ ex9.py
@@ -0,0 +1,18 @@
+# this is other file of my exercices in the course learning python. special characters
+
+
+
+name = "omar david barinas orjuela"
+hello_world = "Hello \n Word! \n Python is Very cool! "
+
+
+print "my name ", name
+print hello_world
+
+
+multi_line = """
+This is a example, of multiline....
+first line
+second line
+third line
+"""
