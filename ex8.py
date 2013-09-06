61b437b86a27ea1b1ccfb14172b10bc309751c8b
Omar David barinas <dbarinas@glive3d.com>
2013-09-05 21:46:57 -0500

    Create ex8.py
    Okay, the formater %r represent the type ...

---
+++ ex8.py
@@ -0,0 +1,11 @@
+# this is the eight exercice of my course "learning python", :) Happy hacking
+
+
+
+
+formater = "%r %r %r %r"
+
+print formater%("Hello","word","formater","print")
+print formater%(1,2,3,4)
+
+# nothing else
